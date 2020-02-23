9c5ea95d528a9d56461eadeed205c23e98c16d13
letterliu <letter0206@gmail.com>
2020-02-23 16:39:56 +0800

    Style: 29 - Countdown Timer

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,8 @@
 
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const audio = document.querySelector('audio');
+const sixtySeconds = 60;
 let countdown = null;
 
 const buttons = document.querySelectorAll('[data-time]');
@@ -14,29 +16,32 @@ const customForm = document.getElementById('custom');
 customForm.addEventListener('submit', handleSubmit);
 function handleSubmit(e) {
   e.preventDefault();
-  const sixtySeconds = 60;
   const totalSeconds = (e.target.minutes.value) * sixtySeconds;
   timer(totalSeconds);
   e.target.reset();
 }
+
 function timer(seconds) {
   clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   displayEndTime(then);
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    if (secondsLeft < 0) {
-      clearInterval(countdown);
-      return;
-    }
-    displayTimeLeft(secondsLeft);
-  }, 16);
+  countdown = setInterval(handleCountdown, 16, then);
+}
+function handleCountdown(then) {
+  const secondsLeft = Math.round((then - Date.now()) / 1000);
+  if (secondsLeft < 0) {
+    clearInterval(countdown);
+    timerDisplay.textContent = `Happy Coding!!!`;
+    audio.play();
+    return;
+  }
+  displayTimeLeft(secondsLeft);
 }
 function displayTimeLeft(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainderSeconds = seconds % 60;
+  const minutes = Math.floor(seconds / sixtySeconds);
+  const remainderSeconds = seconds % sixtySeconds;
   const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
